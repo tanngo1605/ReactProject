6887f9688adc34f0a45fb38a3a0fb25b9d7081f1
tan <nttrongtan1605@gmail.com>
2020-03-21 15:24:28 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import {Route, Switch} from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
-
+/*
+Switch: match the first URL that match
+exact: match the exact URL
+*/
 function App() {
   return (
     <div>
-      <HomePage />
+     < HomePage />
     </div>
   );
 }
