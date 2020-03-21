c2bc39d5021a45aaf458ce8dd5be45c86f431623
tan <nttrongtan1605@gmail.com>
2020-03-21 14:10:25 -0400

    update css

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./menu-item.styles.scss";
 
-
-const MenuItem = ({title}) =>(
-    <div className="menu-item">
-        <div className="content">
-          <h1 className="title">{title}</h1>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-)
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">Shop now</span>
+    </div>
+  </div>
+);
 
 export default MenuItem;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,34 +1,12 @@
 import React from "react";
-import './homepage.styles.scss'
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">Shop now</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span>Shop now</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span>Shop now</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span>Shop now</span>
-        </div>
-      </div>
-    </div>
+  <Directory />
+   
   </div>
 );
 
