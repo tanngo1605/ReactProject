8715f461a27f6c024e12914ceb9b440d9b276279
tan <nttrongtan1605@gmail.com>
2020-04-06 17:52:11 -0400

    done lesson 72

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import {Route, Switch} from 'react-router-dom';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 /*
 Switch: match the first URL that match
 exact: match the exact URL
@@ -10,6 +11,7 @@ exact: match the exact URL
 function App() {
   return (
     <div>
+    <Header/> {/*set outside the Swith so the Header will always be there*/}
     <Switch>
     <Route exact path="/" component={HomePage}></Route>
     <Route exact path="/shop" component={ShopPage}></Route>
