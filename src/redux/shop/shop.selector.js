4db6923f1b1e77312f168c78407b2e0ef4378218
tan <nttrongtan1605@gmail.com>
2020-06-01 16:32:25 -0400

    done section 11

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,5 +1,16 @@
 import {createSelector} from 'reselect';
 
+const COLLECTION_ID_MAP = {
+    hats: 1,
+    sneakers: 2,
+    jackets: 3,
+    womens: 4,
+    men: 5
+
+}
+
+
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -7,3 +18,9 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
+
+export const selectCollection = collectionUrlParam => 
+createSelector (
+    selectCollections,
+    collections => collections.find(collection =>collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+)
